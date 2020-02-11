ff5909c3914c33e6bc5f6f87f1ea5c9bacbf41c0
Kevin <45089457+kevstyers@users.noreply.github.com>
2020-02-10 21:01:33 -0700

    Aww A first C++ Repo
    Learning thuh basics

---
+++ CppNewbie/CppNewbie/CppNewbie.cpp
@@ -0,0 +1,18 @@
+// files to use:
+#include <iostream>
+
+// Libraries :
+using namespace std;
+
+// Main Func
+int main() 
+{
+	//print some text
+	cout << "Hello World" << endl;
+
+	// Wait for it!
+	getchar();
+
+	//END
+	return 0;
+}
